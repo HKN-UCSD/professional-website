400466e1cc3bc75db6bd67cae85ca92f9fb7945b
niannianwang <niw002@ucsd.edu>
2025-10-22 17:19:04 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  base: '/website/', 
+  plugins: [react()],
+  base: '/website/',  // <-- IMPORTANT for GitHub Pages under org/project
 })

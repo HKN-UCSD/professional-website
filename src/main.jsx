3c6fd997a9c6aa42c5c6125fffca46aa86a15a54
niannianwang <niw002@ucsd.edu>
2025-10-29 17:02:18 -0700

    change to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import CareerFair from './pages/CareerFair.jsx'
 import Sponsor from './pages/Sponsor.jsx'
@@ -9,12 +9,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/career-fair" element={<CareerFair />} />
         <Route path="/sponsor" element={<Sponsor />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )

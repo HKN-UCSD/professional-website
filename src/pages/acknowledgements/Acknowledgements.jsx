fafec0913bc45700839e935fb026ded11466a733
Leo Jiang <leojiang62@gmail.com>
2026-04-06 12:33:57 -0700

    Acknowledgements page added :)

--- src/pages/acknowledgements/Acknowledgements.jsx
+++ src/pages/acknowledgements/Acknowledgements.jsx
@@ -2,41 +2,37 @@ import React, { useState, useRef, useEffect } from 'react';
 import './Acknowledgements.css';
 import NavBar from '../../components/NavBar';
 import CarouselCard from './CarouselCard';
-import heroImage from '../../media/images/landing/socials.jpg';
-//^ Placeholder image
-
+import heroImage from '../../media/images/acknowledgements/acknowledgements_title.png';
+import eceLogo from '../../media/images/acknowledgements/logos/ece_logo.png';
+import digikeyLogo from '../../media/images/acknowledgements/logos/digi_key_logo.png';
+import monsterLogo from '../../media/images/acknowledgements/logos/monster.png';
+import qualcommLogo from '../../media/images/acknowledgements/logos/qualcomm.png';
+import ngcLogo from '../../media/images/acknowledgements/logos/ngc_logo.png';
+import morrisImage from '../../media/images/acknowledgements/portraits/morris.jpeg';
+import rubinImage from '../../media/images/acknowledgements/portraits/rubin.jpg';
 /**
  
  */
 
 function Acknowledgements() {
     const slides = [
-        <CarouselCard img={heroImage} title="Card 1" content="This is the content for Card 1." />,
-        <CarouselCard img={heroImage} title="Card 2" content="This is the content for Card 2." />,
-        <div><CarouselCard img={heroImage} title="Card 3" content="This is the content for Card 3." />
-        <CarouselCard img={heroImage} title="Card 4" content="This is the content for Card 4." /></div>,
+        { text: "Faculty", content: <div><CarouselCard img={morrisImage} title="Karcher Morris" content="Assistant Teaching Professor, Electrical and Computer Engineering" />
+        <CarouselCard img={rubinImage} title="Noah Rubin" content="Assistant Professor, Electrical and Computer Engineering" /></div>},
+        { text: "Organizations", content: <div><CarouselCard img={eceLogo} title="UCSD ECE Department" content="Supporting student innovation, technical excellence, and professional development within engineering." />
+        <CarouselCard img={digikeyLogo} title="Digi-Key Electronics" content="Empowering engineers with components, tools, and resources for rapid prototyping and learning." />
+        <CarouselCard img={qualcommLogo} title="Qualcomm" content="Driving innovation in wireless technologies and microcontroller development supporting future engineers through education initiatives." />
+        <CarouselCard img={monsterLogo} title="Monster Energy" content="Fueling creativity, collaboration, and late-night builds." />
+        <CarouselCard img={ngcLogo} title="Northrop Grumman" content="Words." /></div>}
     ];
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const nextSlide = () => {
+   const nextSlide = () => {
         setCurrentIndex((prev) => (prev + 1) % slides.length);
     };
 
     const prevSlide = () => {
         setCurrentIndex((prev) => (prev - 1 + slides.length) % slides.length);
     };
-
-    useEffect(() => {
-     if (slides.length > 1) {
-       const interval = setInterval(() => {
-         setCurrentIndex(
-           (prevIndex) => (prevIndex + 1) % slides.length
-         );
-       }, 5000);
- 
-       return () => clearInterval(interval);
-     }
-   }, [slides.length]);
     
   return (
     <>
@@ -63,42 +59,47 @@ function Acknowledgements() {
                     />
                     {/* Content */}
                     <div 
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-11/12 max-w-4xl"
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-11/12 max-w-6xl"
                     style={{ zIndex: 3 }}
                     >
-                    <h1 className="text-6xl md:text-8xl font-extrabold leading-tight text-white mb-6"
+                    <h1 className="text-6xl md:text-8xl font-extrabold leading-tight text-white mb-6 break-words"
                         style={{
-                            textShadow: `-1px -1px 0 #1E3A8A, 1px -1px 0 #1E3A8A, -1px 1px 0 #1E3A8A, 1px 1px 0 #1E3A8A, -1px 0 0 #1E3A8A, 1px 0 0 #1E3A8A, 0 -1px 0 #1E3A8A, 0 1px 0 #1E3A8A`
+                            textShadow: `-1px -1px 0 #1E3A8A, 1px -1px 0 #1E3A8A, -1px 1px 0 #1E3A8A, 1px 1px 0 #1E3A8A, -1px 0 0 #1E3A8A, 1px 0 0 #1E3A8A, 0 -1px 0 #1E3A8A, 0 1px 0 #1E3A8A`,
                         }}>
                         Acknowledgements
                     </h1>
-                    <p className="text-2xl md:text-3xl text-white"
-                        style={{
-                        textShadow: `-0.5px -0.5px 0 #1E3A8A, 0.5px -0.5px 0 #1E3A8A, -0.5px 0.5px 0 #1E3A8A, 0.5px 0.5px 0 #1E3A8A, -0.5px 0 0 #1E3A8A, 0.5px 0 0 #1E3A8A, 0 -0.5px 0 #1E3A8A, 0 0.5px 0 #1E3A8A`
-                        }}>
-                        Me when I words
-                    </p>
                     </div>
                 </div>
             </header>
 
             <main className="container mx-auto pt-8 pb-16">
-                <div className="carousel-container overflow-hidden">
-                    <div className="flex justify-between w-full h-full pointer-events-none">
-                        <button className="pointer-events-auto" onClick={prevSlide}>《</button>
-                        <h2 className="text-2xl font-bold text-center mb-8 pt-8">Wow I need to make this change too</h2>
-                        <button className="pointer-events-auto" onClick={nextSlide}>》</button>
+                <div className="container overflow-hidden">
+                    <div className="container justify-center pointer-events-none">
+                        <div className="grid grid-cols-3 grid-rows-1">
+                            <button className="justify-self-end text-2xl pointer-events-auto" onClick={prevSlide}>《</button>
+                            {slides.map((slide, index) => (
+                                <h2
+                                key={index}
+                                className={`carousel-item text-xl md:text-3xl font-bold text-center mb-8 pt-8 col-start-2 row-start-1 ${index === currentIndex ? 'active' : ''}`}
+                                >
+                                {slide.text}
+                                </h2>
+                            ))}
+                            <button className="justify-self-start text-2xl pointer-events-auto" onClick={nextSlide}>》</button>
+                        </div>
                     </div>
 
-                    <div className="grid grid-cols-1 grid-rows-1">
-                        {slides.map((slide, index) => (
-                            <div
-                            key={index}
-                            className={`carousel-item pointer-events-none flex items-center justify-center col-start-1 row-start-1 ${index === currentIndex ? 'active' : ''}`}
-                            >
-                            {slide}
-                            </div>
-                        ))}
+                    <div className="container pointer-events-none">
+                        <div className="grid grid-cols-1 grid-rows-1">
+                            {slides.map((slide, index) => (
+                                <div
+                                key={index}
+                                className={`carousel-item justify-center col-start-1 row-start-1 self-start mt-6 flex ${index === currentIndex ? 'active' : ''}`}
+                                >
+                                {slide.content}
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </main>
--- src/pages/acknowledgements/CarouselCard.jsx
+++ src/pages/acknowledgements/CarouselCard.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 
 function CarouselCard({ img, title, content }) {
     return (
-        <div className="flex p-6 w-full max-w-4xl mx-auto" style={{background: 'rgba(0, 0, 0, 0.3)', borderRadius: '12px', border: '1px solid rgba(163, 215, 229, 0.2)'}}>
-            <img className="object-cover w-full rounded-base h-64 md:h-auto md:w-48 mb-4 md:mb-0" src={img} alt={title}/>
-            <div className="card-content">
-                <h5 className="font-bold mb-4 text-heading">{title}</h5>
+        <div className="grid grid-cols-3 flex-wrap items-center p-6 mb-6" style={{background: 'rgba(0, 0, 0, 0.2)', borderRadius: '12px', border: '1px solid rgba(163, 215, 229, 0.2)'}}>
+            <img className="object-cover rounded max-h-64" src={img} alt={title}/>
+            <div className="ps-6 flex flex-col col-span-2" style={{color: '#60A5FA'}}>
+                <h5 className="font-bold mb-4 text-2xl">{title}</h5>
                 <p className="mb-6 text-body">{content}</p>
             </div>
         </div>
